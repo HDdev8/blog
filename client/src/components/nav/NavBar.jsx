3fc877cb372206cc333b661571b5070284b6264f
HDdev8 <114863680+HDdev8@users.noreply.github.com>
2023-06-27 01:04:33 -0400

    improving text link style. Switched file names.

--- client/src/components/nav/NavBar.jsx
+++ client/src/components/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {AppBar as Bar, Toolbar, Container} from "@mui/material";
-import NavMenuFull from "./navMenus/NavMenuFull";
 import NavMenuMobile from "./navMenus/NavMenuMobile";
+import NavMenuFull from "./navMenus/NavMenuFull";
 import UserMenu from "./userMenus/UserMenu";
 
 const NavBar = () => {
@@ -18,8 +18,8 @@ const NavBar = () => {
     <Bar position="static" color="inherit" variant="string" sx={{square: false}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <NavMenuFull handleOpen={handleOpen} handleClose={handleClose} anchor={anchorElement} />
-          <NavMenuMobile handleClose={handleClose} />
+          <NavMenuMobile handleOpen={handleOpen} handleClose={handleClose} anchor={anchorElement} />
+          <NavMenuFull handleClose={handleClose} />
           <UserMenu />
         </Toolbar>
       </Container>
--- client/src/components/nav/navMenus/NavMenuMobile.jsx
+++ client/src/components/nav/navMenus/NavMenuMobile.jsx
@@ -1,4 +1,5 @@
-import {Box, Typography, Button} from "@mui/material";
+import {Box, IconButton, Typography, Menu, MenuItem} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import HdIcon from "@mui/icons-material/Hd";
 import {Link} from "react-router-dom";
 
@@ -7,17 +8,16 @@ const pages = [
   {title: "Users", url: "/api/users"},
 ];
 
-const NavMenuMobile = ({handleClose}) => {
+const NavMenuMobile = ({handleOpen, handleClose, anchor}) => {
   return (
     <>
-      <HdIcon sx={{display: {xs: "flex", md: "none"}, mr: 1}} />
+      <HdIcon sx={{display: {xs: "none", md: "flex"}, mr: 1}} />
       <Typography
-        variant="h5"
+        variant="h6"
         noWrap
         sx={{
           mr: 2,
-          display: {xs: "flex", md: "none"},
-          flexGrow: 1,
+          display: {xs: "none", md: "flex"},
           fontFamily: "monospace",
           fontWeight: 700,
           letterSpacing: ".3rem",
@@ -26,17 +26,46 @@ const NavMenuMobile = ({handleClose}) => {
         }}>
         BLOG
       </Typography>
-      <Box sx={{flexGrow: 1, display: {xs: "none", md: "flex"}}}>
-        {pages.map((page) => (
-          <Button
-            key={page.title}
-            onClick={handleClose}
-            sx={{my: 2, color: "inherent", display: "block"}}
-            component={Link}
-            to={page.url}>
-            {page.title}
-          </Button>
-        ))}
+      <Box sx={{flexGrow: 1, display: {xs: "flex", md: "none"}}}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpen}
+          color="inherit">
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchor}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          open={Boolean(anchor)}
+          onClose={handleClose}
+          sx={{
+            display: {xs: "block", md: "none"},
+          }}>
+          {pages.map((page) => (
+            <MenuItem key={page.title} onClick={handleClose}>
+              <Typography
+                textAlign="center"
+                sx={{textDecorationLine: "none", color: "primary.main"}}
+                variant="button"
+                component={Link}
+                to={page.url}>
+                {page.title}
+              </Typography>
+            </MenuItem>
+          ))}
+        </Menu>
       </Box>
     </>
   );
